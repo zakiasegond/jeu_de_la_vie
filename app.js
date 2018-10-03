0a1a3d0d120772d0650890a0548a15265a512e0b
alexandrakurowski <alex@SIMOCCAUCH21>
2018-10-03 16:20:07 +0200

    affichage de la grille git statusgit status youpi git statusgit status

--- app.js
+++ app.js
@@ -1,27 +1,60 @@
 // console.log ('hello');
 // //creation d'un tableau en 2 dimension
- var grille = new Array(); // on crée le cadre 
+//  var grille = new Array(); // on crée le cadre 
+//  var tab = $('#tab');
+ 
+ 
+// tab.append('<table></table>'); //créer le tableau
+// console.log(tab);
 
-for (var y=0; y<15; y++) // creation de lignes dans la grille
-	grille [y] = new Array(); 
+// // creation de lignes dans la grille
+// for (var y=0; y<15; y++)
+// { 
+// 	var row = tab.append ( '<tr>' + grille [y] + '</tr>');
+
+// console.log(row);
+// }
+
+// 	for (var x=0; x<15; x++) 
+// 	{// creation des cellules
+// 		var cell = tab.apppend ( '<td></td>');
+// 	} 
+
+// on parcourt les lignes et on initilise a zero
+// for (var y=0; y<15; y++)
+// {
+// 		grille[y][x] =0;
+// }
 
 
-for (var y=0; y<15; y++) // on parcourt les lignes
-	for (var x=0; x<15; x++) // creation des cellules
-		grille[y][x] =0;
+var tableau = [];
+var tab = $('#tab');
 
 
-for (var y=0; y<15; y++) // on parcourt les lignes
-	for (var x=0; x<15; x++) // creation des cellules
-		// alert("Case" + y + "-" + x + ":"+ grille[y][x]);
-console.log(grille);
+tab.append('<table></table>'); //créer le tableau
 
+
+// for(i=0; i<15; i++){ // créer ligne de tete (th)
+//     var row = tab.append('<th>'+ tableau[i] + '</th>')
+// }
+
+for(i=0; i<15; i++){ // créer les autre ligne (tr)
+    var row = tab.append('<tr>'+ tableau[i] + '</tr>')
+    
+    // for (j=0; j<15; j++){ // la premiere cellule de chaque ligne (th ou td)
+    //     var cell = row.append('<th>' + tableau[i] + '</th>')
+    // }
+    for(j=0; j<15; j++){ // créer toute les autres cellules vides qui accueilleront les resultats (td ou th)
+        var cellvide = row.append('<td></td>')
+        
+    }
+
+  }  
 
 
 // var tab = $('#tab');
 // var color
 
-// tab.append('<table></table>'); //créer le tableau
 
 
 
