ab5060c153ab4a292bf4485e31b297ba1d4ae814
zakiasegond <segondzakia@live.fr>
2018-10-04 14:04:40 +0200

    cours d'orel

--- app.js
+++ app.js
@@ -50,3 +50,66 @@ var newVegetal = Object.create(vegetaux);
 console.log(newVegetal.tmpReproduct); // Affiche 2
 }
 
+-----------------------------------------------------------------------------------------------------
+cours de Orel : 
+
+// cette fonction renvoie un tableau en 2 dimensions contenant des objets placée aléatoirement
+
+var monde =[];
+
+function genere_monde (monde,x, y, nb_vege, nb_carni, nb_vegetaux){
+    
+    return[
+        [v1, null, av2],
+        [av1, V2, null]
+    ];
+}
+
+// cette fonction genere le html qui permet d'afficher le monde
+
+function afficher_monde(m)
+{
+    $('body').empty();
+}
+
+
+function maj_objets (m)
+{
+    for(y=0; y<15; y++)
+    {
+        for(x=0; x<15; x++)
+        {
+
+            if(m[i][j])
+            {
+                m[i][j].update();
+            } 
+        }      
+    }
+}
+
+
+
+
+
+var monde = genere_monde(monde, 10,10);
+
+
+
+while(true)
+{
+afficher_monde (monde);
+maj_objets(monde);
+
+}
+
+//ou
+//setInterval est une fonction qui va generer le monde automatiquement.
+
+setInterval(function
+{
+    afficher_monde (monde);
+    maj_objets(monde);
+});
+
+
